043256903347457fd4915717067998bacc7b749e
tonyc0213 <62545659+tonyc0213@users.noreply.github.com>
2024-03-25 21:56:49 +0800

    radar

--- Assets/Karting/Scripts/GameModes/TransportObject.cs
+++ Assets/Karting/Scripts/GameModes/TransportObject.cs
@@ -1,31 +1,36 @@
-﻿using UnityEngine;
+﻿using Karting.UI.Radar;
+using UnityEngine;
 
 namespace KartGame.GameModes {
 	public class TransportObject : TargetObject {
 		[Header("TransportTargets")]
 		public TransportTarget transportStart;
+		public Color startIndicatorColor;
 		public TransportTarget transportEnd;
+		public Color endIndicatorColor;
 		
 		void Start() {
 			Register();
 			
 			transportStart.gameObject.SetActive(true);
 			transportStart.OnCollect += OnTransportStart;
+			RadarManager.RegisterTarget?.Invoke(transportStart.transform, startIndicatorColor);
 			
 			transportEnd.gameObject.SetActive(false);
 			transportEnd.OnCollect += OnTransportEnd;
 		}
 
 		void OnTransportStart() {
-			Debug.Log("OnTransportStart");
 			transportStart.gameObject.SetActive(false);
 			transportEnd.gameObject.SetActive(true);
+			
+			RadarManager.UnregisterTarget?.Invoke(transportStart.transform);
+			RadarManager.RegisterTarget?.Invoke(transportEnd.transform, endIndicatorColor);
 		}
 
 		void OnTransportEnd() {
-			Debug.Log("OnTransportEnd");
-			
 			transportEnd.gameObject.SetActive(false);
+			RadarManager.UnregisterTarget?.Invoke(transportEnd.transform);
 			Objective.OnUnregisterPickup(this);
 			
 			TimeManager.OnAdjustTime(TimeGained);
