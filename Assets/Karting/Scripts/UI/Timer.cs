5968a61508aa64f721f6f4c1b46c583fc21bac8c
tonyc0213 <62545659+tonyc0213@users.noreply.github.com>
2024-03-28 20:21:18 +0800

    fix

---
+++ Assets/Karting/Scripts/UI/Timer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.PlayerLoop;
+
+namespace KartGame.UI {
+	public class Timer : MonoBehaviour {
+		public float time;
+		public UnityEvent OnTimeUp;
+
+		float myTime;
+
+		void Update() {
+			myTime -= Time.deltaTime;
+			if (myTime <= 0) {
+				OnTimeUp?.Invoke();
+			}
+		}
+		
+		void OnEnable() {
+			myTime = time;
+		}
+	}
+}
